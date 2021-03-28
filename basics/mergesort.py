a6761ea2a4dcf0e52256b650829655c76f681bb8
sandeepsamaylal <sandeep.samaylal@outlook.com>
2021-03-28 12:08:07 -0700

    Merge-sort

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -52,6 +52,6 @@ if __name__ == "__main__":
         except ValueError as err:
             print("Invalid input.")
             quit(1)
-
+    debug_print(value_list=value_list)
     sorted_list = mergesort(value_list)
     print(sorted_list)
